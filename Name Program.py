fd7d71a2cf6e75ca3fafa3171f7043de3b10cf4f
mateo <matedutten@gmail.com>
2024-01-16 14:05:32 -0300

    first commit

---
+++ Name Program.py
@@ -0,0 +1,8 @@
+# This program says hello and asks for my name
+
+print("Hello World")
+print("how is your name")
+My_Name = input()
+print("It is Good to meet you, " + My_Name)
+print("the length of your name is:")
+print(len(My_Name))
---
+++ Try 1.py
@@ -0,0 +1,7 @@
+Learning = "In Progress"
+if Learning == "In Progress":
+  print("Good Job")
+else:
+    print("Try Again")
+print("Keep Going")
+
