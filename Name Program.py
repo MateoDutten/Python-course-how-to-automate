55850fdb272e183e6073fdc5b7e0c4a9fce0861f
mateo <matedutten@gmail.com>
2024-01-16 14:11:00 -0300

    second commit

--- Name Program.py
+++ Name Program.py
@@ -3,6 +3,6 @@
 print("Hello World")
 print("how is your name")
 My_Name = input()
-print("It is Good to meet you, " + My_Name)
+print("Nice to meet you, " + My_Name)
 print("the length of your name is:")
 print(len(My_Name))
